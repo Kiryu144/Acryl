c1b7045176ecd78691f29c5bda41973f1a1db76a
Kiryu <123davidLP@gmail.com>
2018-11-21 20:29:29 +0100

    Reworked Shader

---
+++ source/engine/opengl/shader/shader.h
@@ -0,0 +1,56 @@
+#ifndef ACRYL_GLSHADERCOMPILER_H
+#define ACRYL_GLSHADERCOMPILER_H
+
+#include <string>
+#include <iostream>
+#include <map>
+#include <GLEW/glew.h>
+#include <GLM/common.hpp>
+#include <GLM/vec2.hpp>
+#include <GLM/vec3.hpp>
+#include <GLM/vec4.hpp>
+#include <GLM/mat4x4.hpp>
+
+#include "../../util/util.h"
+
+namespace Acryl {
+
+class Shader {
+private:
+    static GLuint lastBoundProgramID;
+
+    std::string mVertexShader;
+    std::string mFragmentShader;
+    std::string mGeometryShader;
+    GLuint mProgramID;
+
+    std::map<std::string, GLuint> mUniformLocations;
+
+    GLuint compileAndLink() const;
+    GLuint compileShader(GLenum type, const std::string& src) const;
+    GLuint linkShader(GLuint vertex, GLuint fragment, GLuint geometry = 0) const;
+    void cacheUniformLocations();
+public:
+    Shader(const std::string& vertex, const std::string& fragment, bool isSource = false);
+    Shader(const std::string& vertex, const std::string& fragment, const std::string& geometry, bool isSource);
+
+    Shader(const Shader& other) = delete; //Removes copy constructor
+    void operator=(const Shader& other) = delete; //Removes assingment operator
+
+    void bindProgram() const;
+    GLuint getProgramID() const;
+
+    void setUniform(const std::string& uniform, GLint value) const;
+    void setUniform(const std::string& uniform, GLuint value) const;
+    void setUniform(const std::string& uniform, GLfloat value) const;
+    void setUniform(const std::string& uniform, GLdouble value) const;
+
+    void setUniform(const std::string& uniform, const glm::vec2& value) const;
+    void setUniform(const std::string& uniform, const glm::vec3& value) const;
+    void setUniform(const std::string& uniform, const glm::vec4& value) const;
+    void setUniform(const std::string& uniform, const glm::mat4x4& value) const;
+};
+
+}
+
+#endif
--- source/test/main.cpp
+++ source/test/main.cpp
@@ -1,13 +1,12 @@
 #include "../engine/window/window.h"
-#include "../engine/opengl/shader/glshadercompiler.h"
+#include "../engine/opengl/shader/shader.h"
 #include "../engine/opengl/vbo/vertexbufferobject.h"
 
 
 int main(int argc, char** argv) {
     Acryl::Window window("Acryl!");
 
-    Acryl::GLShaderCompiler shaderCompiler("basic.vert", "basic.frag", false);
-    GLuint programID = shaderCompiler.compileAndLink();
+    Acryl::Shader shader("basic.vert", "basic.frag", false);
 
     float dummyData[] = {
         -0.5f, -0.5f, 0.0f,
@@ -20,7 +19,7 @@ int main(int argc, char** argv) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(programID);
+        shader.bindProgram();
         dummyVBO.bindVertexAttribArray(0);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
